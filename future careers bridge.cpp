e20243c06f0abd085e328232bb360478cd0c6c0a
Lahiruprabhashwara <120106459+Lahiruprabhashwara@users.noreply.github.com>
2022-12-12 20:26:54 +0530

    Add files via upload

---
+++ future careers bridge.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+int math()
+{
+    void class1(){
+    int arr[40];
+    for(int i=1;i<=40;i++)
+    cout<<"enter grades"<<endl;
+    cin>>arr[i];
+    }
+    void class2(){
+    int arr2[40];
+    for(int i=1;i<=40;i++)
+    cout<<"enter grades"<<endl;
+    cin>>arr2[i];
+    }
+    void class3(){
+    int arr3[40];
+    for(int i=1;i<=40;i++)
+    cout<<"enter grades"<<endl;
+    cin>>arr3[i];
+    }
+    void class4(){
+    int arr4[40];
+    for(int i=1;i<=40;i++)
+    cout<<"enter grades"<<endl;
+    cin>>arr4[i];
+    }
+    void class5(){
+    int arr5[40];
+    for(int i=1;i<=40;i++)
+    cout<<"enter grades"<<endl;
+    cin>>arr5[i];
+    }
+    if(class==1){
+        class1();
+    }
+    else if(class==2){
+        class2();
+    }
+    else if(class==3){
+        class3();
+    }
+    else if(class==4){
+        class4();
+    }
+    else{
+    class5();
+    }
+}
+
+int bio(){
+
+    void bclass;
+    cout<<"enter class";
+    cin>>bclass;
+    
+    void bclass1(){
+    int bio1[40];
+    for(int i=1;i<=40;i++)
+    cout<<"enter grades"<<endl;
+    cin>>bio1[i];
+    }
+    void bclass2(){
+    int bio2[40];
+    for(int i=1;i<=40;i++)
+    cout<<"enter grades"<<endl;
+    cin>>bio2[i];
+    }
+    void bclass3(){
+    int bio3[40];
+    for(int i=1;i<=40;i++)
+    cout<<"enter grades"<<endl;
+    cin>>bio3[i];
+    }
+
+}
+
+int main()
+{
+cout<<"bio class marks";
+bio();
+cout<<"math class marks";
+math();
+
+
+
+
+}
